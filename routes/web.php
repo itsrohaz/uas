5fc4cc0595216078c166cab2f6f52580620cec79
itsrohaz <110826618+itsrohaz@users.noreply.github.com>
2022-09-03 10:00:45 +0800

    dua

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,25 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/siswa', function(){
+    echo 'siswa';
+});
+
+//Route::group(['prefix' => 'belajar'], function(){...});
+
+//Route::get('/', function (){
+    //return view(view:'welcome');
+//});
+
+//Route::get('/home', function (){
+    //return view(view:'index');
+//});
+
+//Route::get('/kontak/{hp?}', function ($hp){
+    //return view(view:'kontak', ['handphone' => $hp]);
+//});
+
+//Route::get('/tentang-kami', function (){
+    //return view(view:'tentang');
+//});
